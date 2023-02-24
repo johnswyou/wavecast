c1c09287a716c77990cd94581e3c94d9a09bc533
John You <johnswyou@gmail.com>
2023-02-24 17:35:17 -0500

    Escape all square brackets in calibrateDDM man

--- R/ddm.R
+++ R/ddm.R
@@ -2,15 +2,15 @@
 #' @description
 #' This function calibrates a data-driven model (DDM) for a given input-
 #' output dataset.
-#' @param yc calibration target [N x 1]
-#' @param xc calibration inputs [N x D]
-#' @param ddm data-driven model [string: 'spov','knnr','grnn','rrf']
+#' @param yc calibration target \[N x 1\]
+#' @param xc calibration inputs \[N x D\]
+#' @param ddm data-driven model \[string: 'spov','knnr','grnn','rrf'\]
 #' @param ddm_param data-driven model hyper-parameter(s)
 #' \itemize{
-#'  \item{"spov"}{ddm_param[1] = model order [1 < integer <= 3]}
-#'  \item{"knnr"}{ddm_param[1] = no. of nearest neighbours [1 < integer < sample size-1]}
+#'  \item{"spov"}{ddm_param\[1\] = model order \[1 < integer <= 3\]}
+#'  \item{"knnr"}{ddm_param\[1\] = no. of nearest neighbours \[1 < integer < sample size-1\]}
 #'  \item{"grnn"}{no ddm_param as kernel bandwidth determined automatically}
-#'  \item{"rrf"}{ddm_param[1] = ntrees [(1,500)]}
+#'  \item{"rrf"}{ddm_param\[1\] = ntrees \[(1,500)\]}
 #' }
 #' @return integer, list, etc. that contains calibrated model structure
 #' @details DETAILS
