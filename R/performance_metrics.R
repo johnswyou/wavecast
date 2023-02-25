2df79b42429b50e7bbddb0d37a7bda2dce204eb5
John You <johnswyou@gmail.com>
2023-02-24 19:06:45 -0500

    Add roxygen2 comment above avg.asym.deg.S

--- R/performance_metrics.R
+++ R/performance_metrics.R
@@ -32,33 +32,22 @@ aril <- function(Y, X, alpha=0.5){
 
 # ------------------------------------------------------------------------------
 
+#' @title S index of average asymmetry degree
+#' @description
 #' This function calculates the S index of average asymmetry degree given a
 #' set of predictions 'X', the target 'Y', and a confidence level of
 #' 1 - alpha.
-#'
-#'
-#' Inputs (must be a matrix (i.e., ?as.matrix):
-#' @param Y - target vector (response variable) [N x 1]
-#' @param X - predictions matrix [N x D]
-#' @param alpha - specifies 1 - alpha confidence level [0 < alpha < 1]
-#'
-#' Output:
-#' @param res - S index of average asymmetry degree [scalar]
-
-#' References:
-#'
+#' @param Y target vector (response variable) \[N x 1\]
+#' @param X predictions matrix \[N x D\]
+#' @param alpha specifies 1 - alpha confidence level \[0 < alpha < 1\], Default: 0.5
+#' @return S index of average asymmetry degree \[scalar\]
+#' @references
 #' Xiong, L., N. Wan, X. Wei, and K. O'Connor (2009), Indices
 #' for assessing the prediction bounds of hydrological models
 #' and application by generalised likelihood uncertainty
 #' estimation, Hydrological Sciences Journal, 54:5, 852-871.
-#'
-#' Created on: Dec. 17, 2020 by JMQ
-#' Updated on: Dec. 17, 2020 by JMQ
-#'
-#' Usage:
-#'
-#'
-
+#' @rdname avg.asym.deg.S
+#' @export
 avg.asym.deg.S <- function(Y, X, alpha=0.5){
 
   X = as.matrix(X)
